5819f4a7b3a4fa6c11c8e11e574c3dee0bd22b17
Daniel Wood <daniel.j.wood11@gmail.com>
2018-08-01 10:49:09 -0400

    remove legacy data files

--- data/source/r2csv.R
+++ data/source/r2csv.R
@@ -2,4 +2,8 @@ load("final_data_073118/costs_2015.RData")
 load("final_data_073118/counts_2015.RData")
 load("final_data_073118/race_ethnicity_2015.RData")
 
-write.csv(sco, file="costs_test.csv")
+write.csv(co, file="costs_test.csv")
+write.csv(counts.allstates, file="counts_test.csv")
+write.csv(re, file="race_test.csv")
+
+# From the CSVs, they are turned into JSON using the python scripts (for race and counts) and by "hand" in sublime (batch editing/copying/pasting/searching/etc) for costs
