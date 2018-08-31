00e41a672f38db96e43f876f68df82d04ea9c9d4
Ben Chartoff <bchartoff@gmail.com>
2018-08-31 12:52:10 -0400

    tanh IE polyfill

--- js/model.js
+++ js/model.js
@@ -2,6 +2,13 @@
 
 // define lots of functions
 
+Math.tanh = Math.tanh || function(x){
+    var a = Math.exp(+x), b = Math.exp(-x);
+    return a == Infinity ? 1 : b == Infinity ? -1 : (a - b) / (a + b);
+}
+
+
+
 function getProjEndYear(){
 	return 2025;
 }
