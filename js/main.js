e3f91338a7afd99304cb9cef4ef1286cc4862fdd
Ben Chartoff <bchartoff@gmail.com>
2018-08-31 13:07:17 -0400

    better IE detection function...

--- js/main.js
+++ js/main.js
@@ -14,18 +14,29 @@ window.addEventListener( "pageshow", function ( event ) {
 function IS_IE() {
 
     var ua = window.navigator.userAgent;
-    var msie = ua.indexOf("MSIE ");
 
-    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
-    {
-        alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
-    }
-    else  // If another browser, return 0
-    {
-        alert('otherbrowser');
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
     }
 
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // Edge (IE 12+) => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
     return false;
+
 }
 
 
